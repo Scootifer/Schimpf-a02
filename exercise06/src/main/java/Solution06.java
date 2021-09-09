c2967296265fb5c7e64b51c138e51a52503b57ad
Scootifer <Windmill3341!>
2021-09-09 16:27:37 -0400

    fix

--- exercise06/src/main/java/Solution06.java
+++ exercise06/src/main/java/Solution06.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Solution06 {
+
+import java.util.Scanner;
+import java.util.Date;
+
+/*
+/ The program will request accept in the users age and age they want to retire as ints
+/ Then it will calculate the remaining years to work until retirement
+/ Finally it will use the date package to tell the user what year they wish to retire in
+/
+*/
+public class Solution06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Date d = new Date();
+
+        System.out.print("Please enter your age: ");
+        int age = scan.nextInt();
+        System.out.print("Please enter the age you would like to retire: ");
+        int retire = scan.nextInt();
+        int remainingTime = retire-age;
+
+        int currentYear = d.getYear() + 1900;
+        System.out.println("You will be able to retire in " + remainingTime + " years.");
+        System.out.println("As of " + currentYear + " you will be able to retire in " + (currentYear + remainingTime));
+
+
+    }
 }
